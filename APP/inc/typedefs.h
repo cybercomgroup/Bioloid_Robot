e4f3edbb935b68468a60e9932abecaf001d0c8cc
AntonOlsson <abc2386@gmail.com>
2017-02-02 14:53:04 +0100

    first c project

---
+++ APP/inc/typedefs.h
@@ -0,0 +1,13 @@
+/*
+ * typedefs.h
+ *
+ *  Created on: 2 feb. 2017
+ *      Author: install
+ */
+
+#ifndef APP_INC_TYPEDEFS_H_
+#define APP_INC_TYPEDEFS_H_
+
+// see global.h
+
+#endif /* APP_INC_TYPEDEFS_H_ */
