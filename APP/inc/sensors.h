3a35b21799dcd790295117035872bcdc8ad4f433
khalv <kalle_halvarsson@live.se>
2017-02-23 11:20:15 +0100

    Simplified gyroscope interface

--- APP/inc/sensors.h
+++ APP/inc/sensors.h
@@ -25,54 +25,9 @@
 #define PIN_DMS_MOTP		       GPIO_Pin_8
 #define PIN_DMS_MOTM		       GPIO_Pin_11
 
-#define DEG_TO_RAD				   0.0174532925
-#define RAD_TO_DEG				   57.2957795
-
-#define R_ANGLE			           .3
-#define Q_ANGLE			   	  	   0.002
-#define Q_GYRO				   	   0.1
-
-#define PI						   3.14159265358979323
-
-//Defined for performance reasons
-#define SECONDS_PER_MILLIS 		   0.001
-
 //Used with the ADC multiplexer to select signal source for conversion
 #define PIN_ADC_SELECT0            GPIO_Pin_1
 #define PIN_ADC_SELECT1            GPIO_Pin_2
-
-typedef struct
-{
-  /* These variables represent our state matrix x */
-  double x_angle,
-         x_bias;
-
-  /* Our error covariance matrix */
-  double p_00,
-         p_01,
-         p_10,
-         p_11;
-
-  /*
-   * Q is a 2x2 matrix of the covariance. Because we
-   * assume the gyro and accelerometer noise to be independent
-   * of each other, the covariances on the / diagonal are 0.
-   *
-   * Covariance Q, the process noise, from the assumption
-   *    x = F x + B u + w
-   * with w having a normal distribution with covariance Q.
-   * (covariance = E[ (X - E[X])*(X - E[X])' ]
-   * We assume is linear with dt
-   */
-  double q_angle, q_gyro;
-
-  /*
-   * Covariance R, our observation noise (from the accelerometer)
-   * Also assumed to be linear with dt
-   */
-  double r_angle;
-} gyro;
-
 
 //IR
 
@@ -81,6 +36,5 @@ word read_ir_left(void);
 
 //Gyro
 
-void gyro_setup();
-void gyro_process();
+void gyro_update();
 void gyro_read();
--- APP/src/gyro.c
+++ APP/src/gyro.c
@@ -10,7 +10,6 @@
 #include "printf.h"
 
 /* Globals */
-unsigned long last_read = 0;
 
 word adc_gyro_x = 0;
 word adc_gyro_y = 0;
@@ -21,88 +20,16 @@ word adc_gyro_center_y = 311;
 word gyro_pitch = 0;
 word gyro_roll = 0;
 
-word pitch_angle = 0;
-word roll_angle = 0;
-
-gyro pitch_data;
-gyro roll_data;
-
-/* Function definitions */
-void gyro_init(gyro *g, const float q_angle, const float q_gyro, const float r_angle)
-{
-	g->q_angle = q_angle;
-	g->q_gyro  = q_gyro;
-	g->r_angle = r_angle;
-
-	g->p_00 = 0;
-	g->p_01 = 0;
-	g->p_10 = 0;
-	g->p_11 = 0;
-}
-
-void gyro_setup() {
-	gyro_init(&roll_data, Q_ANGLE, Q_GYRO, R_ANGLE);
-	gyro_init(&pitch_data, Q_ANGLE, Q_GYRO, R_ANGLE);
-	last_read = millis();
-}
-
-void gyro_predict(gyro *g, double dot_angle, double dt)
-{
-	g->x_angle += dt * (dot_angle - g->x_bias);
-	g->p_00 += -1 * dt * (g->p_10 + g->p_01) + dt*dt * g->p_11 + g->q_angle;
-	g->p_01 += -1 * dt * g->p_11;
-	g->p_10 += -1 * dt * g->p_11;
-	g->p_11 += g->q_gyro;
-}
-
-//Kalman update function
-double gyro_update_kalman(gyro *g)
-{
-	const double y = -g->x_angle;
-	const double s = g->p_00 + g->r_angle;
-	const double k_0 = g->p_00 / s;
-	const double k_1 = g->p_10 / s;
-
-	g->x_angle += k_0 * y;
-	g->x_bias  += k_1 * y;
-
-	g->p_00 -= k_0 * g->p_00;
-	g->p_01 -= k_0 * g->p_01;
-	g->p_10 -= k_1 * g->p_00;
-	g->p_11 -= k_1 * g->p_01;
-
-	return g->x_angle;
-}
-
-//This is the funciton you run to get the gyro angles
-void gyro_process()
-{
-	gyro_read();
-	unsigned long now = millis();
-	double dt = ((double)(now - last_read)) * SECONDS_PER_MILLIS;
-
-	// Only process delta angles if at least 1/100 of a second has elapsed
-	if (dt >= 0.01)
-	{
-		last_read = now;
-
- 		// calculate gyro deviations from center values
-		gyro_pitch = adc_gyro_x - (int16) adc_gyro_center_x;
-		gyro_roll = adc_gyro_y - (int16) adc_gyro_center_y;
-
-		gyro_predict(&pitch_data, gyro_pitch * DEG_TO_RAD, dt);
-		gyro_predict(&roll_data, gyro_roll * DEG_TO_RAD, dt);
-
-		pitch_angle = (word) (gyro_update_kalman(&pitch_data)*RAD_TO_DEG);
-		roll_angle = (word) (gyro_update_kalman(&roll_data)*RAD_TO_DEG);
-
-		// TEST:
-		printf("\nPitch: %i, Roll: %i", pitch_angle, roll_angle);
-	}
-}
-
 /* Reads gyroscope sensor data from the ADC.
  * Writes pitch and roll to adc_gyro_x and adc_gyro_y respectively. */
+
+void gyro_update() {
+	gyro_read();
+
+	gyro_pitch += adc_gyro_x - adc_gyro_center_x;
+	gyro_roll += adc_gyro_y - adc_gyro_center_y;
+}
+
 void gyro_read() {
 	/* Start with pitch */
 
